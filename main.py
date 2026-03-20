ed8db395cbc3310616dd5000082e8fe5b6089641
Simreich Somogod <simreich1989@gmail.com>
2026-03-20 14:38:57 +0800

    added app folder

--- main.py
+++ main.py
@@ -3,31 +3,6 @@ from fastapi import FastAPI
 # Run promptly with: fastapi dev main.py
 app = FastAPI()
 
-items = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"},
-    {"id": 3, "name": "Item 3"},
-]
-
-@app.get("/health")
-def health_check():
-    return {"status": "Ok!"}
-
-@app.get("/items")
-def get_items():
-    return items
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    for item in items:
-        if item["id"] == item_id:
-            return item
-    return {"error": "Item not found"}
-
-@app.post("/items")
-def create_item(item: dict):
-    items.append(item)
-    return item
 
 
 
