3df4258bdff94bb5a8fc2358ca25f5f8c64fb1c3
Simreich Somogod <simreich1989@gmail.com>
2026-03-20 14:34:10 +0800

    update post http

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 items = [
-    {"id": 1, "Mio": "Item 1"},
-    {"id": 2, "Suzu": "Item 2"},
-    {"id": 3, "Mina": "Item 3"},
+    {"id": 1, "name": "Item 1"},
+    {"id": 2, "name": "Item 2"},
+    {"id": 3, "name": "Item 3"},
 ]
 
 @app.get("/health")
@@ -24,3 +24,12 @@ def get_item(item_id: int):
             return item
     return {"error": "Item not found"}
 
+@app.post("/items")
+def create_item(item: dict):
+    items.append(item)
+    return item
+
+
+
+
+    
